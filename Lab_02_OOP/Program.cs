c9779175373b0a2d32b0793d2c6385083e9f4196
TexRyte <pavelkononov073@gmail.com>
2025-05-28 19:28:31 +0300

    Add project files.

---
+++ Lab_02_OOP/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+class Program
+{   // Ex 1
+    static double VolumeOfTrianglePrism(double a, double h)
+    {
+        double S = (Math.Sqrt(3) / 4) * a * a;
+        return S * h;
+    }
+
+    static void Main()
+    {
+        try
+        {
+            Console.Write("Введіть a: ");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.Write("Введіть h: ");
+            double h = double.Parse(Console.ReadLine());
+            
+            double S = (Math.Sqrt(3) / 4) * a * a;
+            double V = S * h;
+
+            Console.WriteLine($"Об'єм правильної трикутної призми: {V:F2}");
+            Console.WriteLine($"Об'єм правильної трикутної призми: {VolumeOfTrianglePrism(a, h):F2}");
+        }
+        catch
+        {
+            Console.WriteLine("Некоректне введення!");
+        }
+
+
+
+
+        // Ex 2
+        Console.Write("Введіть x: ");
+        double x = double.Parse(Console.ReadLine());
+
+        if (x > Math.PI / 6 && x < Math.PI / 4)
+        {
+            Console.WriteLine(1 / Math.Cos(x));
+        }
+        else if ( x > Math.PI / 4 && x < Math.PI / 3)
+        {
+            Console.WriteLine(-Math.Tan(x));
+        }
+        else
+        {
+            Console.WriteLine("Некоректне введення!");
+        }
+
+    }
+}
